fd92d41b0e03eb0a7b4665f5294e692e1eac1e1d
jlgoldb2 <jason@datamelon.io>
2014-07-12 16:01:12 -0700

    test_init_app runs, init_app passes

--- flask_meta/test_app.py
+++ flask_meta/test_app.py
@@ -3,5 +3,6 @@ from .app import init_app
 def test_init_app():
     app = init_app()
     appmeta = init_app('appmeta')
-    assert app.name == __name__
+    assert app.name == 'flask_meta.app'
     assert appmeta.name == 'appmeta'
+    assert str(type(app)) == "<class 'flask.app.Flask'>"
