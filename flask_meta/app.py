7e6265f989af27c4e27e7535123334cc751d84c5
jlgoldb2 <jlgoldb2@asu.edu>
2014-07-12 15:21:07 -0700

    added init_app, test_init_app, and pytest

--- flask_meta/app.py
+++ flask_meta/app.py
@@ -1,5 +1,20 @@
-"""
-This is the app that the appmeta will be run from.
-Come on git...
+# -*- coding: utf-8 -*-
 
 """
+This is the app that the appmeta will be run from.
+
+"""
+
+
+from flask import Flask as Flask
+from flask.ext import admin, login, sqlalchemy
+
+def init_app(app_name=__name__):
+    return Flask(app_name)
+
+def test_init_app():
+    app = init_app()
+    appmeta = init_app('appmeta')
+    assert app.name == __name__
+    assert appmeta.name == 'appmeta'
+
